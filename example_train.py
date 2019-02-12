4fd82d6093996d4596e3354412f7077d8f00f314
acharkq <acharkq@gmail.com>
2019-02-12 21:20:50 +0800

    amie

--- example_train.py
+++ example_train.py
@@ -1,5 +1,5 @@
 from rule import rule_mining
 
 if __name__ == '__main__':
-    # rule_mining.main()
-    print(['Rule	Head Coverage	Std Confidence	PCA Confidence	Positive Examples	Body size	PCA Body size	Functional variable	Std. Lower Bound	PCA Lower Bound	PCA Conf estimation'])
+    rule_mining.main()
+    # print(['Rule	Head Coverage	Std Confidence	PCA Confidence	Positive Examples	Body size	PCA Body size	Functional variable	Std. Lower Bound	PCA Lower Bound	PCA Conf estimation'])

177c819f6299f3856d30b98166a7f63ea3f226eb
刘致远 <acharkq@gmail.com>
2019-02-12 20:52:52 +0800

    rule

--- rule/rule_mining.py
+++ rule/rule_mining.py
@@ -91,15 +91,32 @@ usage: java -jar amie_plus.jar [OPTIONS] <TSV FILES>
 '''
 
 
-def mine_single_language_rule_with_amie(path2triples, path2rules):
+def mine_rule_with_amie(path2triples, path2rules):
     '''
     '''
+    import subprocess
     minpca = 0.8
     maxad = 2
     command = 'java -jar amie_plus.jar -maxad %d -minpca %f %s > %s' % (
         maxad, minpca, path2triples, path2rules)
+    res = subprocess.check_output(command.split(' '))
+
+
+
+def mine_rule_for_dbp15k():
+    from project_path import bin_dir
+    bin_dir = bin_dir / 'dbp15k'
+    language_pair_dirs = list(bin_dir.glob('*_en'))
+    for directory in language_pair_dirs:
+        local_bin_dir = directory / 'AMIE'
+        file_paths = local_bin_dir.glob('triples_*.txt')
+        for file_path in file_paths:
+            file_name = file_path.name
+            output_path = local_bin_dir / ('rule_for_' + file_name)
+            mine_rule_with_amie(file_path, output_path)
 
 
 def main():
     from project_path import bin_dir
-    # path = bin_dir / 
+    # path = bin_dir /
+    mine_rule_for_dbp15k()

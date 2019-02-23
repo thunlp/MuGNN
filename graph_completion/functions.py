4af7ce05f9e84406014e68544d823c229f105abc
刘致远 <acharkq@gmail.com>
2019-02-23 19:26:01 +0800

    faster evaluation

--- graph_completion/functions.py
+++ graph_completion/functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import multiprocessing
 from scipy import spatial as spatial
 
 from tools.print_time_info import print_time_info
@@ -12,7 +13,7 @@ def get_hits(sim, top_k=(1, 10, 50, 100)):
     test_num = sim.shape[0]
     # sim = spatial.distance.cdist(Lvec, Rvec, metric='minkowski', p=2)
     # sim = spatial.distance.cdist(Lvec, Rvec, metric='cityblock')
-    def top_get(sim):
+    def top_get(sim, top_k):
         top_x = [0] * len(top_k)
         for i in range(sim.shape[0]):
             rank = sim[i, :].argsort()
@@ -21,8 +22,8 @@ def get_hits(sim, top_k=(1, 10, 50, 100)):
                 if rank_index < top_k[j]:
                     top_x[j] += 1
         return top_x
-    top_lr = top_get(sim)
-    top_rl = top_get(sim.T)
+    top_lr = multiprocess_topk(sim, top_k)
+    top_rl = multiprocess_topk(sim.T, top_k)
     print_time_info('For each source:')
     for i in range(len(top_lr)):
         print_time_info('Hits@%d: %.2f%%' % (top_k[i], top_lr[i] / test_num * 100))
@@ -32,3 +33,39 @@ def get_hits(sim, top_k=(1, 10, 50, 100)):
         print_time_info('Hits@%d: %.2f%%' % (top_k[i], top_rl[i] / test_num * 100))
     # return Hits@10
     return (top_lr[1] / test_num * 100, top_rl[1] / test_num * 100)
+
+
+def multiprocess_topk(sim, top_k=(1, 10, 50, 100)):
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
+    n_p = 4
+
+    # top_x = [0] * len(top_k)
+    def top_get(sim, top_k, id, return_dict, chunk_size):
+        top_x = [0] * len(top_k)
+        s = chunk_size * id
+        for i in range(sim.shape[0]):
+            rank = sim[i, :].argsort()
+            rank_index = np.where(rank == i + s)[0][0]
+            for j in range(len(top_k)):
+                if rank_index < top_k[j]:
+                    top_x[j] += 1
+        return_dict[id] = top_x
+
+    test_num = sim.shape[0]
+    chunk = test_num // n_p + 1
+    sim_chunked = [sim[i:chunk + i, :] for i in range(0, test_num, chunk)]
+    assert len(sim_chunked) == n_p
+    pool = []
+    for i, sim_chunk in enumerate(sim_chunked):
+        p = multiprocessing.Process(target=top_get, args=(sim_chunk, top_k, i, return_dict, chunk))
+        pool.append(p)
+        p.start()
+    for p in pool:
+        p.join()
+    top_x = [0] * len(top_k)
+    for i, top_kk in return_dict.items():
+        assert len(top_x) == len(top_kk)
+        for j in range(len(top_kk)):
+            top_x[j] += top_kk[j]
+    return top_x
--- test.py
+++ test.py
@@ -1,67 +1,63 @@
 import torch
-import torch.nn.functional as F
 import numpy as np
-from scipy import spatial
-from tools.print_time_info import print_time_info
-from torch import nn
-import random
+import multiprocessing
+from graph_completion.torch_functions import torch_l2distance
 
-a = torch.tensor([[1,2,3], [1,3,4]])
-b = torch.tensor([[2,3,0]])
-print(a*b)
-exit()
-a = set(list(range(10))).remove(3)
-print(a)
-exit()
-print(random.sample(a, k=2))
-exit()
-a = set(list(range(10)))
-a = set(str(i) for i in a)
-a.remove('1')
-a.remove('2')
-print(a)
-exit()
-print(loss(b, a, torch.tensor([-1], dtype=torch.float)))
+a = torch.randn((120, 2), dtype=torch.float)
+b = torch.randn((120, 2), dtype=torch.float)
+sim = torch_l2distance(a, b)
+print(sim)
 
-# print(b * a)
-exit()
+def multiprocess_topk(sim, top_k=(1, 10, 50, 100)):
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
+    n_p = 4
 
-def get_hits(sr_embedding, tg_embedding, top_k=(1, 10, 50, 100)):
-    test_num = len(sr_embedding)
-    Lvec = sr_embedding
-    Rvec = tg_embedding
-    sim = spatial.distance.cdist(Lvec, Rvec, metric='cityblock')
-    print(sim)
-    top_lr = [0] * len(top_k)
-    for i in range(Lvec.shape[0]):
+    # top_x = [0] * len(top_k)
+    def top_get(sim, top_k, id, return_dict, chunk_size):
+        top_x = [0] * len(top_k)
+        s = chunk_size * id
+        for i in range(sim.shape[0]):
+            rank = sim[i, :].argsort()
+            rank_index = np.where(rank == i + s)[0][0]
+            for j in range(len(top_k)):
+                if rank_index < top_k[j]:
+                    top_x[j] += 1
+        return_dict[id] = top_x
+
+    test_num = sim.shape[0]
+    chunk = test_num // n_p + 1
+    sim_chunked = [sim[i:chunk + i, :] for i in range(0, test_num, chunk)]
+    assert len(sim_chunked) == n_p
+    pool = []
+    for i, sim_chunk in enumerate(sim_chunked):
+        p = multiprocessing.Process(target=top_get, args=(sim_chunk, top_k, i, return_dict, chunk))
+        pool.append(p)
+        p.start()
+    for p in pool:
+        p.join()
+    top_x = [0] * len(top_k)
+    for i, top_kk in return_dict.items():
+        assert len(top_x) == len(top_kk)
+        for j in range(len(top_kk)):
+            top_x[j] += top_kk[j]
+    return top_x
+
+
+def top_get(sim, top_k=(1, 10, 50, 100)):
+    top_x = [0] * len(top_k)
+    for i in range(sim.shape[0]):
         rank = sim[i, :].argsort()
-        print(rank)
-        print(np.where(rank == i))
-        # exit()
-        rank_index = np.where(rank == i)[0][0]
-        print(rank_index)
-        # exit()
-        for j in range(len(top_k)):
-            if rank_index < top_k[j]:
-                top_lr[j] += 1
-    top_rl = [0] * len(top_k)
-    for i in range(Rvec.shape[0]):
-        rank = sim[:, i].argsort()
         rank_index = np.where(rank == i)[0][0]
         for j in range(len(top_k)):
             if rank_index < top_k[j]:
-                top_rl[j] += 1
-    print_time_info('For each source:')
-    for i in range(len(top_lr)):
-        print_time_info('Hits@%d: %.2f%%' % (top_k[i], top_lr[i] / test_num * 100))
-    print('')
-    print_time_info('For each target:')
-    for i in range(len(top_rl)):
-        print_time_info('Hits@%d: %.2f%%' % (top_k[i], top_rl[i] / test_num * 100))
-    # return Hits@10
-    return (top_lr[1] / test_num * 100, top_rl[1] / test_num * 100)
+                top_x[j] += 1
+    return top_x
 
-a = np.asarray([[1,2,3], [2, 3, 4], [1, 1, 3]])
-b = np.asarray([[1,2,3], [2, 3, 4], [1, 1, 3]])
 
-r = get_hits(a, b)
+
+
+top_x = multiprocess_topk(sim.numpy())
+top_x2 = top_get(sim.numpy())
+print(top_x)
+print(top_x2)
